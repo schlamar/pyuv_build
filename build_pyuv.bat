4af5445eaa014186e69c30b2640303ddf8a19bc3
schlamar <marc.schlaich@gmail.com>
2014-11-25 21:44:52 +0100

    Manual clean up no longer necessary.

--- build_pyuv.bat
+++ build_pyuv.bat
@@ -6,7 +6,6 @@ call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /release /x86
 echo "Building x86"
 py -3.3-32 setup.py build_ext --inplace --libuv-clean-compile
 mv pyuv.pyd pyuv-x86.pyd
-rm -r deps/libuv/Release
 
 call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /release /x64
 echo "Building x64"
