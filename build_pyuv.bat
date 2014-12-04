6911f0c2a409cbc514c1eadaeca9e11c36024a39
schlamar <marc.schlaich@gmail.com>
2014-12-04 18:55:51 +0100

    win: reuse libuv binaries for matching platforms

--- build_pyuv.bat
+++ build_pyuv.bat
@@ -19,15 +19,15 @@ echo "Building 3.3 x86"
 py -3.3-32 setup.py build_ext --libuv-clean-compile bdist_wheel
 mv pyuv.pyd pyuv-x86.pyd
 
+call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /release /x86
+echo "Building 3.4 x86"
+py -3.4-32 setup.py build_ext bdist_wheel
+mv pyuv.pyd pyuv-x86.pyd
+
 call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /release /x64
 echo "Building 3.3 x64"
 py -3.3 setup.py build_ext --libuv-clean-compile bdist_wheel
 
-call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /release /x86
-echo "Building 3.4 x86"
-py -3.4-32 setup.py build_ext --libuv-clean-compile bdist_wheel
-mv pyuv.pyd pyuv-x86.pyd
-
 call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /release /x64
 echo "Building 3.4 x64"
-py -3.4 setup.py build_ext --libuv-clean-compile bdist_wheel
+py -3.4 setup.py build_ext bdist_wheel
